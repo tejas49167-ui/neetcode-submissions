966b9f0bf63174f20fcf3db859b8066d1fa15cb2
tejas49167-ui <tejas49167@gmail.com>
2026-07-26 13:29:18 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+       
+        v = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+        sm = '' 
+        l = 0 
+        
+        for i in s : 
+            if (ord(i)>=ord('a') and ord(i)<=ord('z')) or (ord(i)>=ord('A') and ord(i)<=ord('Z')) or (ord(i)>=ord('0') and ord(i)<=ord('9')) : 
+                sm +=i.lower()
+        r = len(sm) -1 
+        while l <= r : 
+            if sm[l]!=sm[r]: 
+                return False 
+            l +=1 
+            r -=1 
+        return True 
+        
