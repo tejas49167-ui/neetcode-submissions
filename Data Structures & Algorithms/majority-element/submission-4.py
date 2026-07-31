21ae1e6c5b41ff02944a41f40ef40f65675f9c92
tejas49167-ui <tejas49167@gmail.com>
2026-08-01 01:54:27 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums) 
+        candidate = None 
+        count = 0 
+        for i in nums : 
+            if count==0 : 
+                candidate = i 
+                count +=1 
+            elif i==candidate : 
+                count +=1 
+            else : 
+                count -=1 
+            if count > n//2 : 
+                return candidate 
+        return candidate 
