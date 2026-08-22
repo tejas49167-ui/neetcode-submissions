5e05a5e2dcc2181dd50db9e1942b0ffbdf68b120
tejas49167-ui <tejas49167@gmail.com>
2026-08-22 21:49:58 +0530

    Add: special-array-i - submission-4

---
+++ Data Structures & Algorithms/special-array-i/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+
+
+        for i in range(len(nums)-1) : 
+            if (nums[i]+nums[i+1]) % 2 ==0 : 
+                return False 
+        return True 
